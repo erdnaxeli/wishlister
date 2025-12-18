8238b98509fd8dd0ecea12222c4e1de1d8f80c07
Alexandre Morignot <erdnaxeli@cervoi.se>
2025-12-18 09:49:22 +0100

    chore: bump deps

--- pkg/server/app.go
+++ pkg/server/app.go
@@ -31,7 +31,7 @@ func New(config Config) Server {
 	e.Pre(
 		middleware.RemoveTrailingSlashWithConfig(middleware.TrailingSlashConfig{RedirectCode: 308}),
 	)
-	e.Use(middleware.Logger())
+	e.Use(middleware.RequestLogger())
 
 	validate := validator.New(validator.WithRequiredStructEnabled())
 
